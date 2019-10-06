d1b61e91db0e81747637e7a3500fcf70961e0aee
GinaKonidaris <ginakonidaris@gmail.com>
2019-10-06 15:47:53 +0200

    First Commit

---
+++ Task1_18013130GADE/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1_18013130GADE
+{
+    public partial class Form1 : Form
+    {
+        Map map = new Map(20, 20, 20);
+        const int START_X = 20;
+        const int START_Y = 20;
+        const int SPACING = 10;
+        const int SIZE = 20;
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayMap();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtInfo.Text = DateTime.Now.ToLongTimeString();       
+        }
+        private void DisplayMap()
+        {  
+            groupBox1.Controls.Clear();
+            
+        }
+        public void UpdateMap()
+        {
+            foreach (Unit u in map.Units)
+            {
+                if (u.GetType() == typeof(MeleeUnit))
+                {
+                    MeleeUnit n = (MeleeUnit)u;
+                    if (n.Health < 25)
+                    {
+                        switch (r.Next(0, 4))
+                        {
+                            case 0: ((MeleeUnit)u).Move(Direction.North); break;
+                            case 1: ((MeleeUnit)u).Move(Direction.East); break;
+                            case 2: ((MeleeUnit)u).Move(Direction.South); break;
+                            case 3: ((MeleeUnit)u).Move(Direction.West); break;
+                        }
+                    }
+                    else // In comabt or moving towards
+                    {
+                        bool inCombat = false;
+
+                        foreach (Unit e in map.Units)
+                        {
+                            if (u.Inranged(e)) // Incombat
+                            {
+                                u.Combat(e);
+                            }
+                        }
+                        if (!inCombat)
+                        {
+                            Unit c = u.Closest(map.Units);
+
+                            u.Move(n.DirectionTo(c));
+
+                        }
+                    }
+                }
+            }
+        }
+            private void Timer2_Tick(object sender, EventArgs e)
+        {
+            UpdateMap();
+            DisplayMap();
+        }
+        private void Button_click(object sender, EventArgs e)
+        {
+            int x = ((Button)sender).Location.X / SIZE - groupBox1.Location.X / SIZE;
+            int y = ((Button)sender).Location.Y / SIZE - groupBox1.Location.Y / SIZE;
+            foreach (MeleeUnit u in map.Units)
+            {
+
+                if (u.GetType() == typeof(MeleeUnit))
+                {
+                    MeleeUnit n = (MeleeUnit)u;
+                    if (n.Xpos == x && n.Ypos == y)
+                    {
+                        txtInfo.Text = "Button CLicked at" + n.Tostring();
+                    }
+
+                }
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task1_18013130GADE/GameEngine.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1_18013130GADE
+{
+    class GameEngine : Form
+    {
+
+        Map map = new Map(20, 20, 20);
+        const int START_X = 20;
+        const int START_Y = 20;
+        const int SPACING = 10;
+        const int SIZE = 20;
+        Random r = new Random();
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void InitializeMyGroupBox()
+        {
+            // Create and initialize a GroupBox and a Button control.
+            GroupBox groupBox1 = new GroupBox();
+            foreach (Unit u in map.Units)
+            {
+                if (GetType() == typeof(MeleeUnit))
+                {
+                    int start_x; int start_y;
+                    start_x = groupBox1.Location.X;
+                    start_y = groupBox1.Location.Y;
+                    MeleeUnit n = (MeleeUnit)u;
+                    Button b = new Button();
+                    b.Size = new Size(SIZE, SIZE);
+                    b.Location = new Point(START_X + (n.Xpos * SIZE), START_Y + (n.Ypos * SIZE));
+                    b.Text = n.Symbol;
+                    if (n.Team == 1)
+                    {
+                        b.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        b.ForeColor = Color.Green;
+                    }
+                    if (n.Isdead())
+                    {
+                        b.ForeColor = Color.Black;
+                    }
+
+                    b.Click += new EventHandler(btnButton_Click);
+                    groupBox1.Controls.Add(b);
+
+                    this.Controls.Add(b);
+                }
+            }
+
+        }
+        void btnButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+     }
+}
+
+    
+
